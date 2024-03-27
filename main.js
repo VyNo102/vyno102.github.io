12591f44a2a1667e32a09ed84509d3ec35c7ef65
VyNo102 <123734307+VyNo102@users.noreply.github.com>
2024-03-27 20:15:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeColor(newColor) {
+    const elem = document.getElementById("para");
+    elem.style.color = newColor;
+  }
+
+  var obj = {
+    name: 'vyz',
+    age: 21
+  }
+  console.log(name)
